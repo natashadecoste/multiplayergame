30d209314e7ec182d85b0fc3fb96a9547ce2337f
Natasha DeCoste <natasha.decoste@gmail.com>
2019-03-20 17:53:40 -0400

    first commit

---
+++ client/index.js
@@ -0,0 +1,70 @@
+import nipplejs from 'nipplejs';
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+const socket = io();
+
+const manager = createControls();
+var position = {x:0, y:0};
+
+manager.on('added', function (evt, nipple) {
+    nipple.on('start move end dir plain', function (evt, data) {
+        if(evt.type == 'move'){
+            if(data.direction){
+                var x = data.direction.x;
+                if(x == "left" ){
+                    position.x = -1;
+                }
+                else if(x == "right" ){
+                    position.x = 1;
+                }
+
+                var y = data.direction.y;
+                if(y == "up" ){
+                    position.y = -1;
+                }
+                else if(y == "down" ){
+                    position.y = 1;
+                }
+            }
+        }
+        socket.emit('playerMove', position);
+    });
+}).on('removed', function (evt, nipple) {
+    nipple.off('start move end dir plain');
+});
+
+
+socket.emit('newPlayer');
+
+socket.on('state', function(gameState){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let player in gameState.players) {
+        drawPlayer(gameState.players[player])
+    }
+})
+
+
+// drawing the new player
+const drawPlayer = (player) => {
+    ctx.beginPath();
+    ctx.rect(player.x, player.y, player.width, player.height);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+};
+
+
+// creating the joystick
+function createControls(){
+    return nipplejs.create({
+        zone: document.getElementById('joystick-wrapper'),
+        multitouch: false,
+        maxNumberOfNipples: 1,
+        mode: 'dynamic',
+        restOpacity: 1,
+        color: 'black'
+        
+    });
+}
+
+
---
+++ server/app.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = 3000;
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server);
+
+// game logic
+const gameState = {
+  players: {}
+};
+
+app.use(morgan("dev"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Static middleware
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.get("/*", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "index.html"));
+});
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(err.message || "Internal server error");
+});
+
+server.listen(PORT, () => {
+  console.log("Server is live on PORT:", PORT);
+});
+
+// socket responds to things here
+io.on("connection", socket => {
+  console.log("a user connected:", socket.id);
+
+  socket.on("disconnect", function() {
+	console.log("user disconnected");
+	delete gameState.players[socket.id];
+  });
+
+  socket.on("newPlayer", function(){
+	gameState.players[socket.id] = {
+		x: 200,
+		y: 200,
+		width: 20,
+		height: 20 
+	};
+    
+  });
+
+  socket.on('playerMove', function(position){
+	var oldx = gameState.players[socket.id].x;
+	var oldy = gameState.players[socket.id].y;
+	gameState.players[socket.id] = {
+		x: oldx + position.x,
+		y: oldy + position.y,
+		width: 20,
+		height: 20 
+	};
+
+  });
+});
+
+
+// will continuously broadcast the state to the players
+setInterval(() => {
+	io.sockets.emit('state', gameState);
+}, 1000 / 60);
